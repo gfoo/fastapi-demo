e8aea11013547263532b9c1db03e5fc2121b5cfe
Gilles Faucherand <gilles.faucherand@gmail.com>
2022-02-02 16:48:46 +0100

    Return user_id besides token

--- api/authentication.py
+++ api/authentication.py
@@ -33,5 +33,6 @@ def get_token(
     access_token = create_access_token(user_id=user.id)
     return {
         'access_token': access_token,
-        'token_type': 'bearer'
+        'token_type': 'bearer',
+        'user_id': user.id
     }
